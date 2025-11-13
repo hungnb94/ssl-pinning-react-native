5dc02cce7ddbbb150210992377e43438adf5d353
Hung Nguyen <hungnb94@gmail.com>
2025-11-13 12:08:39 +0700

    Load data with react-native-ssl-public-key-pinning

--- src/App.tsx
+++ src/App.tsx
@@ -12,29 +12,44 @@ import {
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 import { useEffect } from 'react';
-import { fetch } from 'react-native-ssl-pinning';
+import { initializeSslPinning } from 'react-native-ssl-public-key-pinning';
+// import { fetch } from "react-native-ssl-pinning";
 
 export const HASH1 = 'sha256/68qDVtVz9DOhHKHKX0clwySoFneRjUa6gRWpBYmsb10=';
 export const HASH2 = 'sha256/kIdp6NNEd8wsugYyyIYFsi1ylMCED3hZbSR8ZFsa/A4=';
 export const HASH3 = 'sha256/mEflZT5enoR1FuXLgYYGqnVEoZvmf9c2bVBpiOjYQ0c=';
+export const PREFIX_LENGTH = 7
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
-  useEffect(() => {
-    console.log('start fetch')
-    fetch('https://finance-api.tekome.com/api/finance/vn30', {
-      method: 'GET',
-      pkPinning: true,
-      sslPinning: {
-        certs: [
-            HASH1,
-            // HASH2,
-            // HASH3,
+
+  const loadData = async () => {
+    await initializeSslPinning({
+      'tekome.com': {
+        includeSubdomains: true,
+        publicKeyHashes: [
+            HASH1.substring(PREFIX_LENGTH),
+            HASH2.substring(PREFIX_LENGTH),
+            HASH3.substring(PREFIX_LENGTH),
         ],
       },
+    });
+
+    console.log('start fetch');
+    fetch('https://finance-api.tekome.com/api/finance/vn30', {
+        // method: 'GET',
+        // pkPinning: true,
+        // sslPinning: {
+        //     certs: [
+        //         HASH0,
+        //     ]
+        // }
     })
       .then(res => console.log('result', res))
       .catch(err => console.warn('error', err));
+  };
+  useEffect(() => {
+    loadData();
     return () => {};
   }, []); // 👈 Empty dependency array = run once
 
